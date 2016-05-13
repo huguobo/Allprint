936f852147bde804549fc581df6596c21e21f297
cooper <huguobo2010@126.com>
2016-05-13 16:32:20 +0800

    Add files via upload

---
+++ MyDLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;//添加DataSet对象的命名空间
+using System.Text;
+
+namespace EvaluatingSystem
+{
+    using System;
+    public class MyDLL
+    {
+        #region  设置DLL文件中静态变量
+        private static DataSet DataCom = new DataSet();
+        #endregion
+
+        #region  设置DLL文件中静态方法
+        /// <summary>
+        /// 接收传递的DataSet对象
+        /// </summary>
+        /// <param Dset="DataSet">DataSet对象</param>
+        public static void TakeOver(DataSet Dset)
+        {
+            DataCom = Dset;//获取DataSet对象
+            Frm_PrintSet FrmPring = new Frm_PrintSet();//实例化打印窗体
+            FrmPring.Show();//显示打印窗体
+        }
+
+        /// <summary>
+        /// 发送传递的DataSet对象
+        /// </summary>
+        public static DataSet SendOut()
+        {
+            return DataCom;
+        }
+        #endregion
+    }
+}
+
